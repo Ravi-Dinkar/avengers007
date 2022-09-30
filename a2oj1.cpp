87de5944866afb464a697d10541ee1b86b028d2b
RAVI KUMAR <73742864+Ravi-Dinkar@users.noreply.github.com>
2022-10-01 00:22:54 +0530

    Add files via upload

---
+++ a2oj1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ int main(){
+     int t;
+     cin>>t;
+     while(t--){
+     string s;
+     cin>>s;
+     int k= s.size();
+     if(k>10){
+         cout<<s[0]<<(k-2)<<s[k-1]<<endl;
+     }
+     else{
+         cout<<s<<endl;
+     }
+
+     }
+
+     
+ }
